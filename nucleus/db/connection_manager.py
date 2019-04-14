4f00f638ec0d5458933f7ef17f47643d5b944e6f
Pruthvi Kumar <pruthvikumar.123@gmail.com>
2019-04-14 14:20:09 +1000

    SQLIte and PGPOOL engines are also now singletons

--- nucleus/db/connection_manager.py
+++ nucleus/db/connection_manager.py
@@ -22,10 +22,12 @@ class ConnectionManager(ConnectionDialects, metaclass=Singleton):
     ConnectionDialects inherit from LogUtilities and ProtonGen. So, those methods can be used in ConnectionManager.
     """
 
-    alchemy_connection_strings = {}
-    alchemy_engine_store = {}
-
     __connection_dialects = ConnectionDialects.dialect_store()
+    __alchemy_connection_strings = {}
+    __alchemy_engine_store = {}
+    __pg_connection_pool = None
+    __sqlite_connection = {}
+
     logger = LogUtilities().get_logger(log_file_name='connectionManager_logs',
                                        log_file_path='{}/trace/connectionManager_logs.log'.format(
                                            ProtonConfig.ROOT_DIR))
@@ -36,15 +38,21 @@ class ConnectionManager(ConnectionDialects, metaclass=Singleton):
 
     @classmethod
     def sqlite_connection_generator(cls):
-        try:
-            with open('{}/proton_vars/proton_sqlite_config.txt'.format(ProtonConfig.ROOT_DIR)) as file:
-                dialect = file.read().replace('\n', '')
-                connection = sqlite3.connect(dialect)
-                return connection
-        except Exception as e:
-            cls.logger.exception(
-                '[connection_manager]: SQLite connection could not be established. Stack trace to follow.')
-            cls.logger.error(str(e))
+        if not bool(cls.__sqlite_connection):
+            try:
+                with open('{}/proton_vars/proton_sqlite_config.txt'.format(ProtonConfig.ROOT_DIR)) as file:
+                    dialect = file.read().replace('\n', '')
+                    cls.__sqlite_connection = sqlite3.connect(dialect)
+                    cls.logger.info('[connection_manager]: SQLITE connection generator invoked for the first time. '
+                                    'Connection successfully generated and maintained at class level.')
+            except Exception as e:
+                cls.logger.exception(
+                    '[connection_manager]: SQLite connection could not be established. Stack trace to follow.')
+                cls.logger.error(str(e))
+        else:
+            cls.logger.info('[connection_manager]: SQLITE connection manager is called subsequently. '
+                            'Connection previously generated will be reused.')
+        return cls.__sqlite_connection
 
     @classmethod
     def __pg_pool(cls):
@@ -52,15 +60,26 @@ class ConnectionManager(ConnectionDialects, metaclass=Singleton):
         ConnectionPool for Postgres governed by psycopg2.
         :return:
         """
-        connection_dialect = cls.__connection_dialects['postgresql']
-        dsn = "dbname='{}' user='{}' host='{}' password='{}' port='{}'".format(connection_dialect['database'],
-                                                                               connection_dialect['user'],
-                                                                               connection_dialect['host'],
-                                                                               connection_dialect['password'],
-                                                                               connection_dialect['port'])
-        # connection_pool with 100 live connections. Tweak this according to convenience.
-        connection_pool = SimpleConnectionPool(1, 100, dsn=dsn)
-        return connection_pool
+        if cls.__pg_connection_pool is None:
+            try:
+                connection_dialect = cls.__connection_dialects['postgresql']
+                dsn = "dbname='{}' user='{}' host='{}' password='{}' port='{}'".format(connection_dialect['database'],
+                                                                                       connection_dialect['user'],
+                                                                                       connection_dialect['host'],
+                                                                                       connection_dialect['password'],
+                                                                                       connection_dialect['port'])
+                # connection_pool with 25 live connections. Tweak this according to convenience.
+                cls.__pg_connection_pool = SimpleConnectionPool(1, 25, dsn=dsn)
+                cls.logger.info('[connection_manager]: PG Pool class method is invoked for first time. '
+                                'PG Pool will be initialized for Postgres engine of PROTON.')
+            except Exception as e:
+                cls.logger.info('[connection_manager]: Error creating a PG Pool. Stack trace to follow.')
+                cls.logger.exception(str(e))
+        else:
+            cls.logger.info('[connection_manager]: Request for PG Pool method is invoked subsequently. '
+                            'PG Pool previously initialized for all PROTON supported engines is returned.')
+
+        return cls.__pg_connection_pool
 
     @classmethod
     @contextmanager
@@ -77,7 +96,7 @@ class ConnectionManager(ConnectionDialects, metaclass=Singleton):
         Returns Engine required by SQL Alchemy ORM.
         :return:
         """
-        if not bool(cls.alchemy_engine_store):
+        if not bool(cls.__alchemy_engine_store):
             import logging
             logging.basicConfig(
                 level=logging.DEBUG,
@@ -88,48 +107,52 @@ class ConnectionManager(ConnectionDialects, metaclass=Singleton):
             )
             logging.getLogger('sqlalchemy.pool').setLevel(logging.DEBUG)
 
-            cls.logger.info('[connection_manager]: alchemy engine class method is invoked for first time. '
+            cls.logger.info('[connection_manager]: Alchemy engine class method is invoked for first time. '
                             'Alchemy engine will be initialized for all PROTON supported engines.')
 
             from sqlalchemy_utils import database_exists, create_database
 
-
             with open('{}/proton_vars/proton_sqlite_config.txt'.format(ProtonConfig.ROOT_DIR)) as file:
                 sqlite_dialect = file.read().replace('\n', '')
-                cls.alchemy_connection_strings['sqlite'] = '{}:///{}'.format('sqlite', sqlite_dialect)
+                cls.__alchemy_connection_strings['sqlite'] = '{}:///{}'.format('sqlite', sqlite_dialect)
 
             for dialect in cls.__connection_dialects:
-                cls.alchemy_connection_strings[dialect] = '{}://{}:{}@{}:{}/{}'.format(dialect,
-                                                                                   cls.__connection_dialects[dialect][
-                                                                                       'user'],
-                                                                                   cls.__connection_dialects[dialect][
-                                                                                       'password'],
-                                                                                   cls.__connection_dialects[dialect][
-                                                                                       'host'],
-                                                                                   cls.__connection_dialects[dialect][
-                                                                                       'port'],
-                                                                                   cls.__connection_dialects[dialect][
-                                                                                       'database']
-                                                                                   )
+                cls.__alchemy_connection_strings[dialect] = '{}://{}:{}@{}:{}/{}'.format(dialect,
+                                                                                         cls.__connection_dialects[
+                                                                                             dialect][
+                                                                                             'user'],
+                                                                                         cls.__connection_dialects[
+                                                                                             dialect][
+                                                                                             'password'],
+                                                                                         cls.__connection_dialects[
+                                                                                             dialect][
+                                                                                             'host'],
+                                                                                         cls.__connection_dialects[
+                                                                                             dialect][
+                                                                                             'port'],
+                                                                                         cls.__connection_dialects[
+                                                                                             dialect][
+                                                                                             'database']
+                                                                                         )
 
-            for connection in cls.alchemy_connection_strings:
-                cls.alchemy_engine_store[connection] = create_engine(cls.alchemy_connection_strings[connection],
-                                                                 pool_size=25, max_overflow=5,
-                                                                 pool_timeout=30, pool_recycle=3600,
-                                                                 poolclass=QueuePool
-                                                                 )
+            for connection in cls.__alchemy_connection_strings:
+                cls.__alchemy_engine_store[connection] = create_engine(cls.__alchemy_connection_strings[connection],
+                                                                       pool_size=25, max_overflow=5,
+                                                                       pool_timeout=30, pool_recycle=3600,
+                                                                       poolclass=QueuePool
+                                                                       )
 
                 # create database if doesnt exist; as per definition in database.ini
-                if not database_exists(cls.alchemy_engine_store[connection].url):
-                    create_database(cls.alchemy_engine_store[connection].url)
+                if not database_exists(cls.__alchemy_engine_store[connection].url):
+                    create_database(cls.__alchemy_engine_store[connection].url)
                     cls.logger.info('[connection_manager]: Proton has created target database in {} as defined in '
                                     'databaseConfig.ini'.format(connection))
 
         else:
-            cls.logger.info('[connection_manager]: alchemy engine class method is invoked subsequently. '
+            cls.logger.info('[connection_manager]: Alchemy engine class method is invoked subsequently. '
                             'Alchemy engine previously initialized for all PROTON supported engines is returned.')
 
-        return cls.alchemy_engine_store
+        return cls.__alchemy_engine_store
 
     @classmethod
     def connection_store(cls):
@@ -159,18 +182,6 @@ class ConnectionManager(ConnectionDialects, metaclass=Singleton):
         connection_manager.update({'mysql': None, 'sqlServer': None})
         return connection_manager
 
-    @staticmethod
-    def __pg_cursor_generator(connection_store):
-        """
-        a simple wrapper on top of __connection_store to help users easily generate cursors without typing much!
-        :return:
-        """
-        if 'postgresql' in connection_store:
-            return connection_store['postgresql']['getCursor'](connection_store['postgresql']['pool'])
-        else:
-            raise Exception('[ConnectionManager]: Connection Store does not contain an entry for postgresql.'
-                            'Check/Debug __connection_store in ConnectionManager.')
-
     @classmethod
     def pg_schema_generator(cls, engine_copy, schema_name):
         try:
@@ -184,11 +195,21 @@ class ConnectionManager(ConnectionDialects, metaclass=Singleton):
             return True
         except Exception as e:
             cls.logger.exception(
-                '[connection_manager]: Error generating schema {} in Postgres. Stack trace to follow.'.format(schema_name))
+                '[connection_manager]: Error generating schema {} in Postgres. Stack trace to follow.'.format(
+                    schema_name))
             cls.logger.error(str(e))
 
-
-
+    @staticmethod
+    def __pg_cursor_generator(connection_store):
+        """
+        a simple wrapper on top of __connection_store to help users easily generate cursors without typing much!
+        :return:
+        """
+        if 'postgresql' in connection_store:
+            return connection_store['postgresql']['getCursor'](connection_store['postgresql']['pool'])
+        else:
+            raise Exception('[ConnectionManager]: Connection Store does not contain an entry for postgresql.'
+                            'Check/Debug __connection_store in ConnectionManager.')
 
 
 if __name__ == '__main__':
