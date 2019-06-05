c9cedb9629c4505794257f3a76c830511d32165e
Pooja Pruthvi <pooja.pruthvikumar@gmail.com>
2019-06-05 14:05:40 +1000

    PR-72 tracked in a different branch. Temporarily operating from here

--- test/Integration/test_sqlite_meta_generator.py
+++ test/Integration/test_sqlite_meta_generator.py
@@ -22,29 +22,12 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from configuration import ProtonConfig
-from nucleus.db.connection_manager import ConnectionManager
-from nucleus.metagen import MetaGen
-from sqlalchemy import MetaData, Table, Column, Integer, DateTime, String, ForeignKey
-from unittest import TestCase
+from test.unit.test_base import TestBase
 
 __author__ = "Pooja Pruthvi, pooja.pruthvikumar@gmail.com"
 __copyright__ = "Copyright (C) 2018 Pooja Pruthvi"
 __license__ = "BSD 3-Clause License"
 __version__ = "1.0"
 
+class TestSqliteMetaGenerator(TestBase):
 
-class TestMetaGen(TestCase):
-
-    def test_metagen(self):
-        metagen_object = MetaGen()
-        assert str(type(metagen_object.metagen_logger)) == "<class 'logging.Logger'>"
-        assert metagen_object._MetaGen__models_root == '{}/mic/models'.format(metagen_object.ROOT_DIR)
-        assert metagen_object._MetaGen__controllers_root == '{}/mic/controllers'.format(metagen_object.ROOT_DIR)
-        assert metagen_object._MetaGen__main_executable == '{}/main.py'.format(metagen_object.ROOT_DIR)
-        assert str(type(metagen_object._MetaGen__jinja_env)) == "<class 'jinja2.environment.Environment'>"
-        assert str(type(metagen_object._MetaGen__models_template)) == "<class 'jinja2.environment.Template'>"
-        assert str(type(metagen_object._MetaGen__controllers_template)) == "<class 'jinja2.environment.Template'>"
-        assert str(type(metagen_object.bootstrap_sqlite)) == "<class 'method'>"
-        assert str(type(metagen_object.new_mic)) == "<class 'method'>"
-        
