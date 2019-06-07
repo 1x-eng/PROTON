a98eba40577d5fff7095b411aa94fc90b192fe5f
PK <pruthvi.kumarbk@anz.com>
2019-06-07 14:07:53 +1000

    CI testing WIP -10

--- test-proton.sh
+++ test-proton.sh
@@ -96,7 +96,7 @@ EOF
     touch ./proton_vars/proton_sqlite_config.txt
     echo "/home/PROTON/proton-db/proton-sqlite.db" >> ./proton_vars/proton_sqlite_config.txt
 
-    docker-compose down && docker-compose up --force-recreate
+    docker-compose down && docker-compose up
 
     echo "PROTON test container is initialized"
 elif [[ ! -z $test ]]; then
