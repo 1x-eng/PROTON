1ca7d48fdd4d888011b7487e4c56631c7ca6d537
PK <pruthvi.kumarbk@anz.com>
2019-11-18 17:26:50 +1100

    Password reset ability tested and functional

--- nucleus/iam/reset.py
+++ nucleus/iam/reset.py
@@ -30,7 +30,6 @@ from nucleus.iam.password_manager import PasswordManager
 from sqlalchemy import MetaData
 from sqlalchemy import select
 from sqlalchemy import Table
-from sqlalchemy.orm import sessionmaker
 
 __author__ = "Pruthvi Kumar, pruthvikumar.123@gmail.com"
 __copyright__ = "Copyright (C) 2018 Pruthvi Kumar | http://www.apricity.co.in"
@@ -47,85 +46,103 @@ class ProtonUserReset(ConnectionManager, PasswordManager, ProtonEmail, MyUtiliti
                                                      log_file_path='{}/trace/iam_user_reset_logs.log'.format(
                                                          self.ROOT_DIR))
 
-    def proton_reset_user(self, db_flavour, schema_name, input_payload):
+    def proton_reset_user(self, db_flavour, schema_name, reset_payload):
         try:
-            reset_payload = input_payload[0]
-            reset_password_metadata = {
-                'username': str,
-                'email': str,
-                'password': str
-            }
-            validation_results = self.validate_proton_payload_type(reset_password_metadata, reset_payload)
+            if isinstance(reset_payload, dict):
+                reset_password_metadata = {
+                    'user_name': str,
+                    'email': str,
+                    'password': str
+                }
+                validation_results = self.validate_proton_payload_type(reset_password_metadata, reset_payload)
 
-            if validation_results['status'] and db_flavour == 'postgresql':
+                if validation_results['status'] and db_flavour == 'postgresql':
 
-                schema_status = self.__alchemy_engine[db_flavour].dialect.has_schema(
-                    self.__alchemy_engine[db_flavour], schema_name)
-                metadata = MetaData(self.__alchemy_engine[db_flavour], reflect=True, schema=schema_name)
-                metadata.reflect(self.__alchemy_engine[db_flavour])
-                connection = self.__alchemy_engine[db_flavour].connect()
-                session_factory = sessionmaker(bind=self.__alchemy_engine[db_flavour])
+                    schema_status = self.__alchemy_engine[db_flavour].dialect.has_schema(
+                        self.__alchemy_engine[db_flavour], schema_name)
+                    metadata = MetaData(self.__alchemy_engine[db_flavour], reflect=True, schema=schema_name)
+                    metadata.reflect(self.__alchemy_engine[db_flavour])
+                    connection = self.__alchemy_engine[db_flavour].connect()
 
-                with connection.begin() as transaction:
-                    if schema_status:
-                        user_registry_table = Table('PROTON_user_registry', metadata)
-                        query_user_id = select([user_registry_table.c.id]).where(
-                            user_registry_table.c.email == reset_payload['email'])
-                        user_id = (connection.execute(query_user_id)).fetchall()
+                    with connection.begin() as transaction:
+                        if schema_status:
+                            user_registry_table = Table('PROTON_user_registry', metadata)
+                            query_user_id = select([user_registry_table.c.id]).where(
+                                user_registry_table.c.email == reset_payload['email'])
+                            user_id = (connection.execute(query_user_id)).fetchall()
 
-                        if len(user_id) == 0:
-                            return json.dumps({
-                                'message': 'Unable to reset password.',
-                                'reason': {
-                                    'message': 'Invalid Email. There is no registered user with that email.'
-                                },
-                                'status': False
-                            })
-                        else:
-                            login_registry_table = Table('PROTON_login_registry', metadata)
-                            query_user_existence = select([login_registry_table.c.user_registry_id]).where(
-                                login_registry_table.c.user_name == reset_payload['username'])
-                            user_existence = (connection.execute(query_user_existence)).fetchall()
-
-                            if len(user_existence) == 0:
+                            if len(user_id) == 0:
                                 return json.dumps({
                                     'message': 'Unable to reset password.',
                                     'reason': {
-                                        'message': 'Invalid Username. Please enter the username and '
-                                                   'email as provided during signup.'
+                                        'message': 'Invalid Email. There is no registered user with that email.'
                                     },
                                     'status': False
                                 })
                             else:
-                                if user_id[0][0] != user_existence[0][0]:
+                                login_registry_table = Table('PROTON_login_registry', metadata)
+                                query_user_existence = select([login_registry_table.c.user_registry_id]).where(
+                                    login_registry_table.c.user_name == reset_payload['user_name'])
+                                user_existence = (connection.execute(query_user_existence)).fetchall()
+
+                                if len(user_existence) == 0:
                                     return json.dumps({
                                         'message': 'Unable to reset password.',
                                         'reason': {
-                                            'message': 'Given email and username do not match. Please enter the '
-                                                       'username and email as provided during signup.'
+                                            'message': 'Invalid Username. Please enter the username and '
+                                                       'email as provided during signup.'
                                         },
                                         'status': False
                                     })
                                 else:
-                                    session = session_factory(bind=connection)
-                                    session.query(login_registry_table).filter_by(
-                                        user_registry_id=user_existence[0][0]).update(
-                                        {'password': self.hash_password(reset_payload['password'])})
-                                    session.commit()
-                                    session.close()
-                                    return json.dumps({
-                                        'message': 'Password reset successful.',
-                                        'status': True
-                                    })
+                                    if user_id[0][0] != user_existence[0][0]:
+                                        return json.dumps({
+                                            'message': 'Unable to reset password.',
+                                            'reason': {
+                                                'message': 'Given email and username do not match. Please enter the '
+                                                           'username and email as provided during signup.'
+                                            },
+                                            'status': False
+                                        })
+                                    else:
+                                        password_update_query = login_registry_table.update().where(login_registry_table.c.user_registry_id==user_existence[0][0]).values(password=self.hash_password(reset_payload['password']))
+                                        password_update_results = (connection.execute(password_update_query))
 
-            else:
-                return json.dumps({
-                    'message': 'Unable to reset password.',
-                    'reason': {
-                        'message': 'Required payload for password reset is: {}'.format(str(reset_password_metadata))
-                    },
-                    'status': False
-                })
+                                        if self.iam_user_reset_logger.info(password_update_results.rowcount) != 0:
+                                            return json.dumps({
+                                                'message': 'Password reset successful.',
+                                                'status': True,
+                                            })
+                                        else:
+                                            return json.dumps({
+                                                'message': 'Password reset unsuccessful due to server side error.',
+                                                'status': False,
+                                            })
+
+                else:
+                    return json.dumps({
+                        'message': 'Unable to reset password.',
+                        'reason': {
+                            'message': 'Required payload for password reset is: {}'.format(str(reset_password_metadata))
+                        },
+                        'status': False
+                    })
+            return json.dumps({
+                'message': 'Unable to reset password.',
+                'reason': {
+                    'message': """POST payload for /reset route must be:
+                    {
+                        "db_flavour": "postgresql",
+                        "reset_payload": { 
+                            "user_name": "<username>",
+                            "email": "<email@email.com>",
+                            "password": "<your new password>"
+                        }
+                    }
+                    """
+                },
+                'status': False
+            })
 
         except Exception as e:
             self.iam_user_reset_logger.exception('[Proton IAM] - Exception while resetting PROTON user password. '
@@ -148,7 +165,7 @@ class IctrlProtonPasswordReset(ProtonUserReset):
                                              'iam',
                                              post_payload['reset_payload'])
 
-            resp.body = json.dumps(results)
+            resp.body = results
             resp.status = falcon.HTTP_201
         except Exception as e:
             resp.body = json.dumps({
